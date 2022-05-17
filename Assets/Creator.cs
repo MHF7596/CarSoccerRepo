23d91424966e34a77a9b28a7edcda8564d594fb7
MHF7596 <mh751792@gmail.com>
2022-05-17 18:38:09 +0430

    05172022

---
+++ Assets/Creator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creator : MonoBehaviour
+{
+
+    public GameObject cube1;
+    public GameObject cube2;
+    public int number;
+
+    void Start()
+    {
+        for(int i=0; i<number; i++)
+        {
+            Instantiate(cube1, new Vector3(transform.position.x+2*i,0,0), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    float speed = 120;
+    void Update()
+    {
+        transform.Rotate(Vector3.right, speed * Time.deltaTime, Space.Self);
+    }
+}
